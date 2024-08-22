c088350b7d387da964e0910e5f812acc3348d4fb
rchu <rchu@glimsol.com>
2024-08-22 09:47:05 +0800

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://whatsapp-glimsol-default-rtdb.asia-southeast1.firebasedatabase.app/',  // Realtime Database URL
+  databaseURL: 'https://whatsapp-glimsol-2cd5f-default-rtdb.asia-southeast1.firebasedatabase.app/',  // Realtime Database URL
 });
 
 const db = admin.database();  // Get a reference to the Realtime Database
